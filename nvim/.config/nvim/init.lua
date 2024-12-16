f9a0efc1398dc0bee539caec4f59abbdbfba54c9
heyGio <giovanni.affatato@mail.polimi.it>
2024-12-16 14:56:45 +0100

    Added save keymap

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -13,6 +13,7 @@ vim.g.mapleader = ' '
 
 vim.keymap.set('n', '<leader><CR>', ':so ~/.config/nvim/init.lua<CR>') 
 vim.keymap.set('n', '<leader>pv', ':Vex<CR>')
+vim.keymap.set('n', '<leader>s', ':w<CR>')
 
 
 -- add C-p to search file in git, leader pf project files 
