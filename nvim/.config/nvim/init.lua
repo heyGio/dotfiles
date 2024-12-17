6d516e2290942d6cc7f32a422727a3bb093a186b
heyGio <giovanni.affatato@mail.polimi.it>
2024-12-17 11:33:41 +0100

    Added linting, formatting, debugger

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,7 +2,6 @@ require("config.lazy")
 
 vim.cmd("set scrolloff=8")
 
-
 vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.cmd("set tabstop=4 softtabstop=4")
@@ -11,7 +10,7 @@ vim.cmd("set expandtab")
 vim.cmd("set smartindent")
 vim.g.mapleader = ' '
 
-vim.keymap.set('n', '<leader><CR>', ':so ~/.config/nvim/init.lua<CR>') 
+vim.keymap.set('n', '<leader><CR>', ':so ~/.config/nvim/init.lua<CR>')
 vim.keymap.set('n', '<leader>pv', ':Vex<CR>')
 vim.keymap.set('n', '<leader>s', ':w<CR>')
 
@@ -19,3 +18,12 @@ vim.keymap.set('n', '<leader>s', ':w<CR>')
 -- add C-p to search file in git, leader pf project files 
 -- nnoremap <C-j> :cnext<CR>
 -- nnoremap <C-k> :cprev<CR>
+
+-- Highlight when yanking text
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true}),
+    callback = function()
+    vim.highlight.on_yank()
+    end,
+})
--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,19 +1,56 @@
 return {
-    { 'williamboman/mason.nvim', opts = { } },
-    { 'williamboman/mason-lspconfig.nvim', opts = {
-        ensure_installed = { 'lua_ls', 'pyright' }
-        }
-    },
-    { 'neovim/nvim-lspconfig',
-        config = function()
-            local lspconfig = require('lspconfig')
-            lspconfig.lua_ls.setup({})
-            lspconfig.pyright.setup({})
+	{ "williamboman/mason.nvim", opts = {} },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = { "lua_ls", "pyright", "basedpyright" },
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"folke/lazydev.nvim",
+				ft = "lua", -- only load on lua files
+				opts = {
+					library = {
+						-- See the configuration section for more details
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+		},
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            -- Use LspAttach to activate key bidning only when buffer is connected to server
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-        end
-    }
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities
+			})
+			lspconfig.basedpyright.setup({
+				capabilities = capabilities,
+				settings = {
+					pyright = {
+						-- Using Ruff's import organizer
+						disableOrganizeImports = true,
+					},
+					python = {
+						analysis = {
+							-- Ignore all files for analysis to exclusively use Ruff for linting
+							ignore = { "*" },
+						},
+					},
+				},
+			})
+
+			lspconfig.ruff.setup({
+				capabilities = capabilities
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
