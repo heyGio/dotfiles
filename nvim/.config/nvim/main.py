43e7b0557e41589b118f04323455dd8af6417723
heyGio <giovanni.affatato@mail.polimi.it>
2024-12-20 17:20:04 +0100

    Added luarocks

--- nvim/.config/nvim/main.py
+++ nvim/.config/nvim/main.py
@@ -1,6 +1,23 @@
+import time
+
+import numpy as np
+
+
+def fun(bello: int) -> int:
+    x = 1
+
+    if bello == 1:
+        return 0
+    else:
+        return x + bello
+
+
 def main():
-    print("Hello World!")
+    variable = [0, 1, 2, 3]
+
+    for i in range(len(variable)):
+        print(fun(variable[i]))
+
 
 if __name__ == "__main__":
     main()
-
